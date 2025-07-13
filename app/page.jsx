e7fdf729b22b21b990d281707f30818ad5f830d1
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-07-13 11:36:39 +1000

    react three fiber type gradient

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
+import Blops from "@/components/Blops";
 import CSSGradient from "@/components/CSSGradient";
+import ThreeDShapes from "@/components/ThreeDShapes";
 
 export default function Home() {
   return (
     <main>
       <CSSGradient />
+      <Blops />
+      <ThreeDShapes />
     </main>
   );
 }
---
+++ components/Blops.jsx
@@ -0,0 +1,9 @@
+const Blops = () => {
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <div className="blob absolute" />
+    </div>
+  )
+}
+
+export default Blops
